a049e93a1387634142eecaca6c2bd7351f4e8d6b
Pancake Rula <pancakerula@gmail.com>
2022-08-01 22:00:02 -0700

    did some work forgot what i did

--- 1_AM/drills.js
+++ 1_AM/drills.js
@@ -77,10 +77,31 @@ const shiftRight = function(str, num) {
   // Example = shiftThree("Hello") === "lloHe"
 }
 
-const tokenize = function(str) {
+function tokenize(str) {
   // TODO - write a function which converts a multi-word string into an array of words
-}
+  // well uh assuming you meant words with spaces i could probably attempt that
 
+
+  // set a counter
+  let i = 0;
+  // set a scattered and organized into words array
+  let scattered = str.split('');
+  let neatWords = [];
+  // while counter isn't string length
+  while(i != str.length){
+    // if the char is not a space
+    if (scattered[i] != " "){
+      neatWords.push(scattered[i])
+    }else{
+      neatWords.splice(" ")
+      
+    }
+    i+= 1
+  }
+  return neatWords
+
+}
+console.log(tokenize("What do I do"))
 const uniqueOnes = function(ary) {
   // TODO - write a function which returns the inputted array without duplicate elements
 }
@@ -94,23 +115,56 @@ const zip = function(arr1, arr2) {
   let zipArray = [];
   // if array length isn't equal
   if (arr1.length != arr2.length) {
-    return "ZIP: " + -1
+    return -1
   // but if the array length is equal 
   }else{
+    // set the counter i to 0
     let i = 0;
-    while (i != arr1.length){
-      zipArray.push(arr1[i] + ", " + arr2[i])
+    // while i is less then the first array's length, 
+    // but it doesn't matter since first and second are 
+    // now confirmed equal
+    while (i < arr1.length){
+      // push the first array and the second array based off of 
+      // which order so like first and first, second and second
+      zipArray.push(" " + arr1[i] + ", " + arr2[i] );
+      // then you just add one to i so it isn't stuck on a while loop
       i += 1
     } 
-
-    return "ZIP: " + zipArray
+    // then you return the array
+    return zipArray
   }
   
 }
-console.log(zip([1,2,3],[5,3,1]))
+console.log("ZIP: " + zip([1,2,3],[5,3,1 ]))
+
+
 const unzip = function(arr) {
   // TODO - Write a function which does the opposite of `zip()`
+
+  // first you take one arrray and you you uh split 
+  // it into two pieces yeyah
+  let arr1 = []
+  let arr2 = []
+  // now this isn't going to work unless you uh idk 
+  // so you make sure it's even ok
+  if (arr.length % 2 == 0){
+    // you'll want to loop this the string length amount of times
+    let i = 0;
+    while (i < arr.length){
+      arr1.push(" " + arr[i]);
+      i += 1
+      arr2.push(" " + arr[i] );
+      i += 1
+    }
+    // once that's done you return the arr 1 and 2
+    return "ARRAY 1:" + arr1 + " || ARRAY 2:" + arr2
+
+  // but like if it isn't even then you return -1 cause i have no idea
+  }else{
+    return -1
+  }
 }
 
+console.log("UNZIP: " + unzip([1,2,3,4]))
 // Write tests here:
 
